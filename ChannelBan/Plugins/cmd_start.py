496b25fe12d905136dcd4fcc638c75f00944552a
HyDrix777 <94365103+HyDrix777@users.noreply.github.com>
2022-03-22 15:40:31 +0530

    Update cmd_start.py

--- ChannelBan/Plugins/cmd_start.py
+++ ChannelBan/Plugins/cmd_start.py
@@ -1,26 +1,14 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-@Client.on_message(filters.command(["start", "start@ChannelBanRobot"]))
+@Client.on_message(filters.command(["start"]))
 async def start(_, message: Message):
     await message.reply_text(
-        f"""<b>𝑯𝒆𝒚 {message.from_user.first_name} 👋😝!</b>
- `𝑯𝒆𝒚𝒂 𝑰'𝒎 𝑨 𝑨𝒏𝒕𝒊 𝑪𝒉𝒂𝒏𝒏𝒆𝒍 𝑻𝒆𝒍𝒆𝒈𝒓𝒂𝒎 𝒃𝒐𝒕 𝒕𝒐 𝒅𝒆𝒍𝒆𝒕𝒆 𝒂𝒏𝒅 𝒃𝒂𝒏 𝒎𝒆𝒔𝒔𝒂𝒈𝒆 𝒔𝒆𝒏𝒕 𝒃𝒚 𝒄𝒉𝒂𝒏𝒏𝒆𝒍`""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "📢 Channel", url="https://t.me/Tg_Galaxy"
-                    ),
-                    InlineKeyboardButton(
-                        "➕Add Me To Group➕", url="http://t.me/AntiChannelBan_x2bot?startgroup=botstart"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "🔻Clone Owner🔻", url="https://t.me/HydraLivegrambot"
-                    )
-                ]
-            ]
+        f"""<b>Hᴇʏ {message.from_user.first_name}!</b>
+Jᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ᴛʜᴇ ᴄʜᴀᴛ, ᴀɴᴅ I ᴡɪʟʟ ʙʟᴏᴄᴋ ᴛʜᴇ ᴄʜᴀɴɴᴇʟs ᴛʜᴀᴛ ᴡʀɪᴛᴇ ᴛᴏ ᴛʜᴇ ᴄʜᴀᴛ""",
+        reply_markup=InlineKeyboardMarkup( [[
+           InlineKeyboardButton("📢 Channel", url="https://t.me/Tg_Galaxy"),
+           InlineKeyboardButton("➕Add Me To Group➕", url="http://t.me/AntiChannelBan_x2bot?startgroup=botstart")
+           ]]
         )
     )
