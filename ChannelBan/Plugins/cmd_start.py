c5a67662b3b2b35b5877632a12f6abf82f9b1b72
HyDrix777 <94365103+HyDrix777@users.noreply.github.com>
2022-03-28 15:56:15 +0530

    Update cmd_start.py

--- ChannelBan/Plugins/cmd_start.py
+++ ChannelBan/Plugins/cmd_start.py
@@ -1,8 +1,15 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.errors import UserNotParticipant
+
+
+
+force_subhydra = "songdownload_group"
+
+
 
 @Client.on_message(filters.private & filters.command(["start"]))
-async def start(_, message: Message):
+async def start(bot, message: Message):
     await message.reply_text(
         f"""<b>Hᴇʏ {message.from_user.first_name}!</b>
 Jᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ᴛʜᴇ ᴄʜᴀᴛ ᴀɴᴅ ᴘʀᴏᴍᴏᴛᴇ ᴛᴏ me ᴀᴅᴍɪɴ, ᴛʜᴇɴ I ᴡɪʟʟ ʙʟᴏᴄᴋ ᴛʜᴇ ᴄʜᴀɴɴᴇʟs ᴛʜᴀᴛ ᴡʀɪᴛᴇ ᴛᴏ ᴛʜᴇ ᴄʜᴀᴛ""",
